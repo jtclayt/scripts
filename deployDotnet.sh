efdcd8d5cdf4ce76bcb8e9d9098b54a9566e3d2b
jtclayt <jt.clayton92@yahoo.com>
2020-08-17 12:34:06 -0700

    Update deploy script

--- deployDotnet.sh
+++ deployDotnet.sh
@@ -2,6 +2,11 @@
 # Author: Justin Clayton
 # This script automates building an ASP.NET project with a mysql database.
 
+if [ $# -lt 1 ]; then
+  echo "Need a project name" 1>&2
+  exit 1
+fi
+
 # Setup database
 cd ..
 dotnet restore
@@ -11,5 +16,7 @@ dotnet ef database update
 # Setup supervisor
 sudo apt install supervisor -y
 dotnet publish
+sudo touch "/etc/supervisor/conf.d/$1.conf"
+sudo chmod 766 "/etc/supervisor/conf.d/$1.conf"
 sudo cat deploy/supervisor.txt > "/etc/supervisor/conf.d/$1.conf"
 sudo service supervisor restart
